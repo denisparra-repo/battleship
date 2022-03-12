bcaed66df78f58ca279b37f048da91d6a7203c22
Denis Parra <denis.parra.89@hotamil.com>
2022-03-12 19:04:05 -0400

    first comit

---
+++ src/battleship.js
@@ -0,0 +1,56 @@
+function sum(a, b) {
+    return a + b;
+}
+let board = [];
+let numberOfPlayers = 0;
+function createBoard(n , m) {
+   for (let i = 0; i < n; i++) {
+       let row = [];
+       for (let j = 0; j<m; j++) {
+           row.push('-');
+       }
+       board.push(row)
+   }
+   console.log(board)
+}
+
+function getRowLength() {
+    return board.length;
+}
+
+function getColumnLength() {
+    return board[0].length;
+}
+
+function setShip(...positions) {
+    for(let i = 0; i < positions.length; i++) {
+        const position = positions[i];
+        board[position[0]][position[1]] = 'S';
+    }
+}
+function isShipThere(position) {
+    if (board[position[0]][position[1]] === 'S') {
+        return true;
+    }
+}
+
+function createPlayers(players) {
+    const player1 = players[0];
+    const player2 = players[1];
+    numberOfPlayers = 2;
+}
+
+function getNumberOfPlayers() {
+    return numberOfPlayers;
+}
+
+module.exports = {
+    sum,
+    createBoard,
+    getRowLength,
+    getColumnLength,
+    setShip,
+    isShipThere,
+    createPlayers,
+    getNumberOfPlayers,
+};
---
+++ test/battleship.test.js
@@ -0,0 +1,27 @@
+const {sum, createBoard, getColumnLength, getRowLength, setShip, isShipThere, createPlayers,getNumberOfPlayers  } = require('../src/battleship')
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('create a board 5 * 5', () => {
+    createBoard(5,5);
+    expect(getRowLength()).toBe(5)
+    expect(getColumnLength()).toBe(5)
+});
+
+test('set ship on the board', () => {
+    createBoard(5,5);
+    setShip([0,0], [0,1]);
+    setShip([4,0], [3,0], [2,0]);
+    expect(isShipThere([0,0])).toBeTruthy();
+    expect(isShipThere([0,1])).toBeTruthy();
+    expect(isShipThere([4,0])).toBeTruthy();
+    expect(isShipThere([3,0])).toBeTruthy();
+    expect(isShipThere([2,0])).toBeTruthy();
+});
+
+test('get players', () => {
+    createPlayers(['player1', 'player2']);
+    expect(getNumberOfPlayers()).toBe(2);
+});
